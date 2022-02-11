d77b2ea2dddad83b412bd1fe12a4c42c334d0b63
Jarkko Passi <jarkko.passi@tuni.fi>
2022-02-11 09:46:42 +0200

    Osa4 a tehty

---
+++ utils/list_helper.js
@@ -0,0 +1,20 @@
+const dummy = () => {
+  // ...
+}
+
+module.exports = {
+  dummy
+}
+
+const totalLikes = (blogs) => {
+  let total = 0
+  blogs.forEach(element => {
+    total += element.likes
+  })
+  return total
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
